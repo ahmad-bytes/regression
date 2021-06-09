c836d82179ff5381fdeb9d2744bf65dfbb16d034
Bilal Ahmad <mailtobilalahmad@yahoo.com>
2021-06-08 23:48:09 -0400

    hw draft q1 first 4 parts

--- Hw_2_Q1.R
+++ Hw_2_Q1.R
@@ -8,8 +8,6 @@ hd<-0
 m = as.matrix(HW2_data)
 print(m)
 
-n = nrow(m)
-
 # get the y vector
 y <- m[,1]
 #print(y)
@@ -18,6 +16,9 @@ y <- m[,1]
 xx <- m[,2:4]
 #print(x)
 
+n = nrow(xx)
+p = ncol(xx)
+
 
 #this is transpose of X multiplied by x and inverse
 xi<-solve(t(xx) %*% xx)
@@ -25,14 +26,15 @@ xi<-solve(t(xx) %*% xx)
 
 #this Beta Hat, the hat matrix multiplied by y matrix
 bh<-xi %*%t (xx) %*% y
+#1.1a
 print(bh)
 
 # for distribution of beta 3 
 index = 3
-View(xi)
-c3 <- xi[index,index]
-print(c3)
-
+#View(xi)
+cindex <- xi[index,index]
+#1.1b
+print(cindex)
 
 # calculated yh using the estimated parameters
 yh<-xx %*% bh
@@ -40,18 +42,53 @@ yh<-xx %*% bh
 
 # calculate residual vector
 eh <- y - yh
-
 #print(eh)
+ehTeh = t(eh) %*% eh
+
+# lets calculate sigma, we need this for 
+# 1/(n-p) * e
+sigma <- (t(eh) %*% eh) / (n-p)
+print(sigma)
+
+xxreduced <- m[,2:3]
+q = ncol(xxreduced)
+xireduced <- solve(t(xxreduced) %*% xxreduced)
+bhreduced <- xireduced %*% t(xxreduced) %*% y
+yhreduced <- xxreduced %*% bhreduced
+ehreduced <- y - yhreduced
+ehreducedTehreduced = t(ehreduced) %*% ehreduced
+Fstats = ((ehreducedTehreduced - ehTeh) / (p-q)) / ((ehTeh) / (n-p))
+df1 = p-q
+df2 = n-p
+#degree of freedom 1 : 1, 27, F stats 1.35, critical value: 7.674 - hypothesis is not rejected
+
+#marginal distribution of b3
+#1.3
+bindex <- bh[index,1]
+Tstats <- (bindex / (cindex)^0.5) / (ehTeh / (n-p)^0.5)
+# Tstats is 0.266 
+# p value is 0.39 - hypothesis is not rejected
+
+#calculating leverage score
 
 # calculate hat matrix -- x multiple by xi(x transpose * x and inverse) multiple by x matrix
 ht <- xx %*% xi %*% t(xx)
+h11 <- ht[1,1]
+h22 <- ht[2,2]
 
 # take the diagonal elements of hat matrix, which will give us the leverage scores
-hd<-diag(ht)
-
+#hd<-diag(ht)
 #print(hd)
+#h11 = 0.05, h22 = 0.10
+#h22 is greater, high leverage is high influence
 
-# this will give the studetized residual matrix
+
+
+
+
+
+
+# this will give the studentized residual matrix
 for(i in 1:n)
 {
   es[i] <- eh[i]/sqrt(1-hd[i])
