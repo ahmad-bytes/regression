b2f8491259e2847bbda77c6a1a9860d3eaa54621
Bilal Ahmad <mailtobilalahmad@yahoo.com>
2021-07-22 21:15:19 -0400

    save hw 7

--- Hw_7_Q1.R
+++ Hw_7_Q1.R
@@ -3,6 +3,8 @@ HW7_data <- read.csv("C:/Users/bilal/Dropbox/MS/RPractice/HomeWork/HW7_data.csv"
 #print summary so we can verify file is loaded
 summary(HW7_data)
 
+class(HW7_data)
+
 # function is y = e(tx)
 # first derivative is te(tx)
 # second derivative is t^2e(tx)
@@ -12,75 +14,60 @@ summary(HW7_data)
 y <- 0
 xx <- 0
 
-m = as.matrix(HW7_data)
+#m = as.matrix(HW7_data)
 # cut the y vector which is the first column
-y <- m[,1]
+#y <- m[,1]
 # cut the x matrix which is the second column
-xx <- m[,2]
+#xx <- m[,2]
 
-n = length(xx)
-
-
-l_theta <- function (theta, x_i)
-{
-  l_theta <- as.vector(exp(theta * x_i))
-}
+y <- HW7_data$y
+xx <- HW7_data$x
 
 
-l_theta_dash <- function (theta, x_i)
-{
-  l_theta_dash  <- as.vector(theta * exp(theta * x_i))
-}
-
-l_theta_dash_dash <- function (theta, x_i)
-{
-  l_theta_dash_dash <- as.vector(theta^2 * exp(theta * x_i))
-}
+n = length(y)
 
 t <- 1
+u <- 0
+d <- 0
 
-for(k in 2:100)
+for(k in 2:10)
 {
   # numerator
-  u <- sum((y - l_theta(t, xx)) %*% l_theta_dash(t,xx))
+  u <- sum(xx * exp(t * xx) * (exp(t * xx) - y))
   # denominator
-  d <- sum((y %*% l_theta_dash_dash(t, xx)) - (l_theta_dash(t,xx))^2 - (l_theta(t,xx) %*% l_theta_dash_dash(t,xx)))
+  d <- sum(xx^2 * exp(t * xx) * (2 * exp(t * xx) - y))
   t <- t - u/d
 }
 
-yh<-l_theta(t, xx)
 
-sigma_squared = (sum(y - yh)^2) / (n-1)
+yh<-exp(t * xx)
+eh <- y - yh
+
+sigma_squared = sum ((y - yh)^2) / (n-1)
 sigma = sqrt(sigma_squared)
-sigma_theta = sigma_squared * sum (l_theta_dash(t, xx)^2)/  sum(l_theta_dash_dash(t,xx)^2)
+
 
 ttt <- 0
 bbb <- 0
 yyy <- 0
-xxx <- 0
 
 xxx <- xx
 for(kk in 1:1000)
 {
-  yyy <- 0
-  #yyy <- l_theta(t, xx) + rnorm(n, 0, sigma)
-  #yyy <- l_theta(t, xx) + 0.1 * rnorm(n, 0, sigma)
-  yyy <- l_theta(t, xx) + (sigma * 0.1 * rnorm(n))
-  
+  yyy <- exp(t * xx) + (sigma * rnorm(n))
   ttt <- 1
   
-  for(k in 1:100)
+  for(k in 2:10)
   {
     # numerator
-    u <- sum((yyy - l_theta(ttt, xxx)) %*% l_theta_dash(ttt,xxx))
+    u <- sum(2 * xx * exp(ttt * xx) * (exp(ttt * xx) - yyy))
     # denominator
-    d <- sum((yyy %*% l_theta_dash_dash(ttt, xxx)) - (l_theta_dash(ttt,xxx))^2 - (l_theta(ttt,xxx) %*% l_theta_dash_dash(ttt,xxx)))
+    d <- sum(2 * xx^2 * exp(ttt * xx) * (2 * exp(ttt * xx) - yyy))
     ttt <- ttt - u/d
   }
-  
-  #bbb[kk] <- l_theta(ttt, xxx)
   bbb[kk] <- ttt
 }
 
 var(bbb, na.rm = TRUE)
 summary(bbb)
+
--- Hw_7_Q1v2.R
+++ Hw_7_Q1v2.R
@@ -3,10 +3,6 @@ HW7_data <- read.csv("C:/Users/bilal/Dropbox/MS/RPractice/HomeWork/HW7_data.csv"
 #print summary so we can verify file is loaded
 summary(HW7_data)
 
-# function is y = e(tx)
-# first derivative is te(tx)
-# second derivative is t^2e(tx)
-
 # cut x and y vectors from data
 
 y <- 0
@@ -18,56 +14,71 @@ y <- m[,1]
 # cut the x matrix which is the second column
 xx <- m[,2]
 
-n = length(xx)
+n = length(y)
 
+t <- 1
+u <- 0
+d <- 0
 
-l_theta <- function (theta, x_i)
-{
-  l_theta <- exp(theta * x_i)
-}
-
-
-l_theta_dash <- function (theta, x_i)
-{
-  l_theta_dash <- theta * exp(theta * x_i)
-}
-
-l_theta_dash_dash <- function (theta, x_i)
-{
-  l_theta_dash_dash <- theta^2 * exp(theta * x_i)
-}
-
-t <- 0.99
-
-yh <- l_theta(t, xx)
-
-sigma_squared = (sum(y - yh)^2) / (n-1)
-sigma = sqrt(sigma_squared)
+t <- 0.99025
+sigma <- 1.0051
+sigma_squared <- 1.0103
 
 ttt <- 0
 bbb <- 0
 yyy <- 0
-
 mean_xx = mean(xx)
 
+log_y = mean_xx * t
 
-for(kk in 1:1000)
+for(kk in 1:10000)
 {
-  yyy <- l_theta(t, mean_xx) + sigma * rnorm(n)
-  
-  ttt[1] <- 1
+  yyy <- exp(t * mean_xx) + (1 * rnorm(n))
+  ttt <- 1
   
   for(k in 2:10)
   {
     # numerator
-    u <- sum((yyy - l_theta(ttt[k-1], xx)) * l_theta_dash(ttt[k-1],xx))
+    u <- sum(2 * xx * exp(ttt * xx) * (exp(ttt * xx) - yyy))
     # denominator
-    d <- sum((yyy * l_theta_dash_dash(ttt[k-1], xx)) - (l_theta_dash(ttt[k-1],xx))^2 - (l_theta(ttt[k-1],xx) * l_theta_dash_dash(ttt[k-1],xx)))
-    ttt[k] <- ttt[k-1] - u/d
+    d <- sum(2 * xx^2 * exp(ttt * xx) * (2 * exp(ttt * xx) - yyy))
+    ttt <- ttt - u/d
   }
-  
-  bbb[kk] <- l_theta(ttt[k], mean_xx)
+  bbb[kk] <- (ttt * mean_xx)
 }
 
-var(log(bbb))
-sqrt(var(log(bbb)))
+var_bbb <- var(bbb, na.rm = TRUE)
+sigma_bbb = sqrt(var_bbb)
+
+log_y
+var_bbb
+sigma_bbb
+sd(bbb)
+
+
+yyy <- 0
+ddd <- 0
+
+for(kk in 1:10000)
+{
+  yyy <- (t * mean_xx) + (1 * rnorm(n))
+  ttt <- 1
+  
+  for(k in 2:10)
+  {
+    # numerator
+    u <- sum(xx * ((ttt * xx) - yyy))
+    # denominator
+    d <- sum(xx^2)
+    ttt <- ttt - u/d
+  }
+  ddd[kk] <- ttt * mean_xx
+}
+
+var_ddd <- var(ddd, na.rm = TRUE)
+sigma_ddd = sqrt(var_ddd)
+sd_ddd = sd(ddd)
+
+log_y
+var_ddd
+sigma_ddd
